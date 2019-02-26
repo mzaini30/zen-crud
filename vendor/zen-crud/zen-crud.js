30f5b141edd10a01e29babedbb447dbe27512fcf
mzaini30 <muhzaini30@gmail.com>
2019-02-26 15:47:03 +0800

    oke

--- vendor/zen-crud/zen-crud.js
+++ vendor/zen-crud/zen-crud.js
@@ -2,5 +2,20 @@ zen_init = function(table, data){
 	for (n in data){
 		localStorage.setItem(table + '_' + data[n], '[]')
 	}
-	localStorage.setItem(table + '_' + 'id', '[]')
+	localStorage.setItem(table + '_id', '[]')
+}
+
+zen_create = function(table, data){
+	// masih masalah jika table_id kosong
+	ambil_id = JSON.parse(localStorage.getItem(table + '_id'))
+	id_baru = Number(ambil_id[ambil_id.length - 1]) + 1
+	ambil_id.push(id_baru)
+	localStorage.setItem(table + '_id', JSON.stringify(ambil_id))
+
+	field = []
+	for (n in data){
+		field[n] = JSON.parse(localStorage.getItem(table + '_' + n))
+		field[n].push(data[n])
+		localStorage.setItem(table + '_' + n, JSON.stringify(field[n]))
+	}
 }
